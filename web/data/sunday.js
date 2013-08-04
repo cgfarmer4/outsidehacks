65ab74bb19835bbee010a5c0ffeb9feeff9d3e4f
Chase Farmer <cfarmer@fanhattan.com>
2013-08-04 10:35:11 -0700

    converted all data

--- web/data/sunday.js
+++ web/data/sunday.js
@@ -1,269 +1,235 @@
 var sunday = [
 	{ 
-		"stage": "Lands End",
+		"stage": [312, 660],//"Lands End",
 		"artists": [
 			{
 				"name" : "Ivan Neville's Dumpstaphunk",
-				"start_time" : "12:10",
-				"end_time": "1:00",
-				"duration": "50",
-				"hottness": "0.333307"				
+				"start_time" : 10, //"12:10",
+				"end_time": 60, //"1:00",
+				"hottness": 0.333307				
 			},
 			{
 				"name" : "Fishbone",
-				"start_time" : "1:30",
-				"end_time": "2:20",
-				"duration": "40",
-				"hottness": "0.454942"				
+				"start_time" : 90, //"1:30",
+				"end_time": 2*60+20, //"2:20",
+				"hottness": 0.454942				
 			},
 			{
 				"name" : "Foals",
-				"start_time" : "2:50",
-				"end_time": "3:50",
-				"duration": "60",
-				"hottness": "0.682176"				
+				"start_time" : 60*2+50, //"2:50",
+				"end_time": 60*3+50, //"3:50",
+				"hottness": 0.682176				
 			},
 			{
 				"name" : "Daryl Hall and John Oates",
-				"start_time" : "4:20",
-				"end_time": "5:20",
-				"duration": "60",
+				"start_time" : 60*4+20, //"4:20",
+				"end_time": 60*5+20, //"5:20",
 				"hottness": "0.36525"				
 			},
 			{
 				"name" : "Vampire Weekend",
-				"start_time" : "5:50",
-				"end_time": "7:00",
-				"duration": "70",
-				"hottness": "0.791765"				
+				"start_time" : 60*5+50, //"5:50",
+				"end_time": 60*7, //"7:00",
+				"hottness": 0.791765
 			},
 			{
 				"name" : "Red Hot Chili Peppers",
-				"start_time" : "7:45",
-				"end_time": "9:35",
-				"duration": "110",
-				"hottness": "0.76647"				
+				"start_time" : 60*7+45, //"7:45",
+				"end_time": 60*9+35, //"9:35",
+				"hottness": 0.76647
 			}
 		]
 	},
 	{ 
-		"stage": "Sutro",
+		"stage": [498, 206],//"Sutro",
 		"artists": [
 			{
 				"name" : "The Easy Leaves",
-				"start_time" : "12:00",
-				"end_time": "12:45",
-				"duration": "45",
-				"hottness": "0.485892"				
+				"start_time" : 0, //"12:00",
+				"end_time": 45, //"12:45",
+				"hottness": 0.485892	
 			},
 			{
 				"name" : "Camper Van Beethoven",
-				"start_time" : "1:15",
-				"end_time": "2:00",
-				"duration": "45",
-				"hottness": "0.471959"				
+				"start_time" : 60+15, //"1:15",
+				"end_time": 60*2, //"2:00",
+				"hottness": 0.471959		
 			},
 			{
 				"name" : "Kurt Vile",
-				"start_time" : "2:30",
-				"end_time": "3:20",
-				"duration": "40",
-				"hottness": "0.667695"				
+				"start_time" : 60*2+30, //"2:30",
+				"end_time": 60*3+20, //"3:20",
+				"hottness": 0.667695			
 			},
 			{
 				"name" : "Trombone Shorty and Orleans Avenue",
-				"start_time" : "3:50",
-				"end_time": "4:40",
-				"duration": "50",
-				"hottness": "0.485179"				
+				"start_time" : 60*3+50, //"3:50",
+				"end_time": 60*4+40, //"4:40",
+				"hottness": 0.485179				
 			},
 			{
 				"name" : "Dawes",
-				"start_time" : "5:10",
-				"end_time": "6:00",
-				"duration": "50",
-				"hottness": "0.546896"				
+				"start_time" : 60*5+10, //"5:10",
+				"end_time": 60*6, //"6:00",
+				"hottness": 0.546896
 			},
 			{
 				"name" : "Willie Nelson and Family",
-				"start_time" : "6:30",
-				"end_time": "7:40",
-				"duration": "70",
-				"hottness": "0.647403"				
+				"start_time" : 60*6+30, //"6:30",
+				"end_time": 60*7+40, //"7:40",
+				"hottness": 0.647403
 			}
 		]
 	},
 	{ 
-		"stage": "Twin Peaks",
+		"stage": [1775, 365],//"Twin Peaks",
 		"artists": [
 			{
 				"name" : "Little Green Cars",
-				"start_time" : "12:40",
-				"end_time": "1:20",
-				"duration": "30",
-				"hottness": "0.647542"				
+				"start_time" : 40, //"12:40",
+				"end_time": 80, //"1:20",
+				"hottness": 0.647542
 			},
 			{
 				"name" : "Rudimental",
-				"start_time" : "2:05",
-				"end_time": "2:50",
-				"duration": "45",
-				"hottness": "0.798229"				
+				"start_time" : 60*2+5, //"2:05",
+				"end_time": 60*2+50, //"2:50",
+				"hottness": 0.798229				
 			},
 			{
 				"name" : "Emeli Sandé",
-				"start_time" : "3:35",
-				"end_time": "4:25",
-				"duration": "40",
-				"hottness": "0.844917"				
+				"start_time" : 60*3+35, //"3:35",
+				"end_time": 60*4+25, //"4:25",
+				"hottness": 0.844917				
 			},
 			{
 				"name" : "A-Trak",
-				"start_time" : "5:10",
-				"end_time": "6:00",
-				"duration": "50",
-				"hottness": "0.552412"				
+				"start_time" : 60*5+10, //"5:10",
+				"end_time": 60*6, //"6:00",
+				"hottness": 0.552412				
 			},
 			{
 				"name" : "Matt and Kim",
-				"start_time" : "6:45",
-				"end_time": "7:35",
-				"duration": "40",
-				"hottness": "0.564115"				
+				"start_time" : 60*6+45, //"6:45",
+				"end_time": 60*7+35, //"7:35",
+				"hottness": 0.564115				
 			},
 			{
 				"name" : "Kaskade",
-				"start_time" : "8:25",
-				"end_time": "9:35",
-				"duration": "70",
-				"hottness": "0.648649"				
+				"start_time" : 60*8+25, //"8:25",
+				"end_time": 60*9+35, //"9:35",
+				"hottness": 0.648649				
 			}
 		]
 	},
 	{ 
-		"stage": "Panhandle",
+		"stage": [1407, 435],//"Panhandle",
 		"artists": [
 			{
 				"name" : "The Wild Feathers",
-				"start_time" : "12:00",
-				"end_time": "12:40",
-				"duration": "40",
-				"hottness": "0.335409"				
+				"start_time" : 0, //"12:00",
+				"end_time": 40, //"12:40",
+				"hottness": 0.335409				
 			},
 			{
 				"name" : "Kopecky Family Band",
-				"start_time" : "1:20",
-				"end_time": "2:00",
-				"duration": "40",
-				"hottness": "0.49315"				
+				"start_time" : 80, //"1:20",
+				"end_time": 60*2, //"2:00",
+				"hottness": 0.49315				
 			},
 			{
 				"name" : "Deap Vally",
-				"start_time" : "2:50",
-				"end_time": "3:30",
-				"duration": "40",
-				"hottness": "0.61763"				
+				"start_time" : 60*2+50, //"2:50",
+				"end_time": 60*3+30, //"3:30",
+				"hottness": 0.61763				
 			},
 			{
 				"name" : "King Tuff",
-				"start_time" : "4:25",
-				"end_time": "5:05",
-				"duration": "40",
-				"hottness": "0.492065"				
+				"start_time" : 60*4+25, //"4:25",
+				"end_time": 60*5+5, //"5:05",
+				"hottness": 0.492065				
 			},
 			{
 				"name" : "MS MR",
-				"start_time" : "6:00",
-				"end_time": "6:40",
-				"duration": "40",
-				"hottness": "0.682856"				
+				"start_time" : 60*6, //"6:00",
+				"end_time": 60*6+40, //"6:40",
+				"hottness": 0.682856				
 			},
 			{
 				"name" : "Dillon Francis",
-				"start_time" : "7:35",
-				"end_time": "8:20",
-				"duration": "45",
-				"hottness": "0.497679"				
+				"start_time" : 60*7+35, //"7:35",
+				"end_time": 60*8+20, //"8:20",
+				"hottness": 0.497679				
 			}
 		]
 	},
 	{ 
-		"stage": "The Dome by Heineken",
+		"stage": [676, 544],//"The Dome by Heineken",
 		"artists": [
 			{
 				"name" : "Griffin Camper",
-				"start_time" : "12:00",
-				"end_time": "1:30",
-				"duration": "90",
-				"hottness": "0.26289"				
+				"start_time" : 0, //"12:00",
+				"end_time": 60+30, //"1:30",
+				"hottness": 0.26289				
 			},
 			{
 				"name" : "Sleight of Hands",
-				"start_time" : "1:30",
-				"end_time": "3:00",
-				"duration": "90",
-				"hottness": "0.341333"			
+				"start_time" : 90, //"1:30",
+				"end_time": 60*3, //"3:00",
+				"hottness": 0.341333			
 			},
 			{
 				"name" : "WhiteNoize",
-				"start_time" : "3:00",
-				"end_time": "4:30",
-				"duration": "90",
-				"hottness": "0.4604"
+				"start_time" : 60*3, //"3:00",
+				"end_time": 60*4+30, //"4:30",
+				"hottness": 0.4604
 			},
 			{
 				"name" : "Gene Farris",
-				"start_time" : "4:30",
-				"end_time": "6:00",
-				"duration": "90",
-				"hottness": "0.402086"				
+				"start_time" : 60*4+30, //"4:30",
+				"end_time": 60*6, //"6:00",
+				"hottness": 0.402086				
 			},
 			{
 				"name" : "DJ Pierre",
-				"start_time" : "6:00",
-				"end_time": "90",
-				"duration": "7:30",
-				"hottness": "0.436159"				
+				"start_time" : 60*6, //"6:00",
+				"end_time": 60*7+30, //"90",
+				"hottness": 0.436159			
 			}
 		]
 	},
 	{ 
-		"stage": "The Barbary",
+		"stage": [1051, 330],//"The Barbary",
 		"artists": [
 			{
 				"name" : "Bring The Rock w/Greg Behrendt & the Reigning Monarchs w/Matt Braunger, Dave Hill, Bruce McCulloch, Thao Nguyen and Nick Thune",
-				"start_time" : "12:30",
-				"end_time": "1:40",
-				"duration": "70",
-				"hottness": ".35"			
+				"start_time" : 30, //"12:30",
+				"end_time": 60+40, //"1:40",
+				"hottness": .35			
 			},
 			{
 				"name" : "Craig Robinson hosted by Morgan Murphy",
-				"start_time" : "2:00",
-				"end_time": "3:15",
-				"duration": "75",
-				"hottness": ".35"				
+				"start_time" : 60*2, //"2:00",
+				"end_time": 60*3+15, //"3:15",
+				"hottness": .35				
 			},
 			{
 				"name" : "Crash Test with Rob Huebel and guests Natasha Leggero, Joe Mande and Bruce McCulloch",
-				"start_time" : "3:45",
-				"end_time": "4:55",
-				"duration": "70",
-				"hottness": ".35"				
+				"start_time" : 60*3+45, //"3:45",
+				"end_time": 60*4+55, //"4:55",
+				"hottness": .35				
 			},
 			{
 				"name" : "Craig Robinson hosted by Nick Thune",
-				"start_time" : "5:15",
-				"end_time": "6:30",
-				"duration": "75",
-				"hottness": ".35"
+				"start_time" : 60*5+15, //"5:15",
+				"end_time": 60*6+30, //"6:30",
+				"hottness": .35
 			},
 			{
 				"name" : "Singin' & Pingin': DJ Purple + American Tripps Present Berlin-Style Ping-Pong + Karaoke Dance Party",
-				"start_time" : "6:30",
-				"end_time": "8:00",
-				"duration": "90",
-				"hottness": ".35"				
+				"start_time" : 60*6+30, //"6:30",
+				"end_time": 60*8, //"8:00",
+				"hottness": .35				
 			}
 		]
 	}

95e132492d90e5274492c455ea24cc9d449a4d61
Chase Farmer <cfarmer@fanhattan.com>
2013-08-04 01:40:09 -0700

    updated json data

--- lib/echonest.rb
+++ lib/echonest.rb
@@ -4,35 +4,36 @@ require 'json'
 API_KEY = 'HHGHYPQC0OMN8TCOA'
 
 # Echonest API 
-# artist = Echonest::Artist.new(API_KEY, 'Weezer')
+artist = Echonest::Artist.new(API_KEY, 'Kurt Vile')
+puts artist.hotttnesss
 
 #Day
-day = 'sunday'
-# Filename
-filename = 'schemas/' + day + '.json'
+# day = 'sunday'
+# # Filename
+# filename = 'schemas/' + day + '.json'
 
-# Read in JSON of artists
+# # Read in JSON of artists
 
-schedule = JSON.parse(IO.read(filename))
+# schedule = JSON.parse(IO.read(filename))
 
-# Inject artists back into hash
-schedule[day].each { |x|  
+# # Inject artists back into hash
+# schedule[day].each { |x|  
 
-	puts "X: #{x}"
+# 	puts "X: #{x}"
 
-	x["artists"].each{ |y|
+# 	x["artists"].each{ |y|
 
-		begin
-		  artist = Echonest::Artist.new(API_KEY, y["name"])
-		rescue Echonest::Error => e
-			"Something happened with echonest, keep trucking #{e}"
-		end
+# 		begin
+# 		  artist = Echonest::Artist.new(API_KEY, y["name"])
+# 		rescue Echonest::Error => e
+# 			"Something happened with echonest, keep trucking #{e}"
+# 		end
 
-		puts "Hotness for #{y["name"]}, #{artist.hotttnesss}"
-	}
+# 		puts "Hotness for #{y["name"]}, #{artist.hotttnesss}"
+# 	}
 	
 
-}
+# }
 
 # Write out hash back to file
 

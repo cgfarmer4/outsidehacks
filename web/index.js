467b2d43d8fa09f8f69cd8a68c49d712f19ec551
Jason Hsu <jason@isotix.com>
2013-08-04 01:28:59 -0700

    Push

--- web/index.js
+++ web/index.js
@@ -1,123 +1,70 @@
+
+var image = new Image();
+var canvas = null;
+var heatmap = null;
+
+var Pointer = {
+	mapS : 1, mapX : 0, mapY : 0,
+	prvX : 0, prvY : 0,
+	ptrX : 0, ptrY : 0,
+	begX : 0, begY : 0
+};
+
 $(window).load(function()
 {
-	var canvas = document.getElementById("canvas");
+	image.src = 'map.png';
+
+	canvas = document.getElementById("canvas");
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
 
-	var heatmap = heatmapFactory.create({"element": "heatmap"});
+	var drag = false;
+
+	$('#heatmap').on('touchstart mousedown', function(e) {
+		drag = true;
+		Pointer.begX = e.clientX;
+		Pointer.begY = e.clientY;
+
+		Pointer.ptrX = Pointer.begX * 1/Pointer.mapS - Pointer.mapX;
+		Pointer.ptrY = Pointer.begY * 1/Pointer.mapS - Pointer.mapY;
+	});
+	$('#heatmap').on('touchend mouseup', function(e) {
+		drag = false;
+		Pointer.prvX = Pointer.mapX;
+		Pointer.prvY = Pointer.mapY;
+	});
+	$('#heatmap').on('touchmove mousemove', function(e) {
+		var touches = e.targetTouches;
+		if(touches.length == 2) {
+			var x = Math.pow(touches[0].clientX - touches[1].clientX, 2);
+			var y = Math.pow(touches[0].clientY - touches[1].clientY, 2);
+			console.log(Math.sqrt(x + y));
+		}
+		Pointer.mapX = (e.clientX - Pointer.begX) * 1/Pointer.mapS + Pointer.prvX;
+		Pointer.mapY = (e.clientY - Pointer.begY) * 1/Pointer.mapS + Pointer.prvY;
+		if(drag) {
+			var g = canvas.getContext('2d');
+			g.clearRect(0, 0, canvas.width, canvas.height);
+			g.save();
+			g.scale(Pointer.mapS, Pointer.mapS);
+			g.translate(Pointer.mapX, Pointer.mapY)
+			g.drawImage(image, 0,0);
+			g.restore();
+		}
+	});
+	
 
 
+	heatmap = heatmapFactory.create({"element": "heatmap"});
 	var centerX = canvas.width / 2;
 	var centerY = canvas.height / 2;
 	var radius = 70;
 
 	heatmap.store.setDataSet({ max: 10, data: [{x: centerX, y: centerY, count: radius}]});
 
-	var context = canvas.getContext('2d');
-
 	$('#slider').hide();
 	$('#slider').change(function() {
 		var val = $(this).val() == 0 ? 12 : $(this).val();
 		$('#time').html(val + " P.M.");
 	});
-
-	/*
-	context.beginPath();
-	context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-	context.fillStyle = 'green';
-	context.fill();
-	context.lineWidth = 5;
-	context.strokeStyle = '#003300';
-	context.stroke();
-*/
-
-
-/*
-	expand = function(e) {
-		var item = $(this).parent().find(".item-more");
-		item.toggle();
-		if (item.is(":visible")) {
-			$(this).html('less');
-		} else {
-			$(this).html('more');
-		}
-	}
-
-	play_pause = function(e) {
-		if($(this).data("play") == "play") {
-			$(this).data("play", "pause");
-			$(this).html("&#10074;&#10074;")
-		} else {
-			$(this).data("play", "play");
-			$(this).html("&#9654;")
-		}
-	}
-
-	highlight = function(e) {
-		// set all the other background colors regular
-		$('.item-container').css('background-color', '#FFF');
-		$('.item-container').css('color', '#000');
-		$('.location').css('color', '#333')
-		$('.date').css('color', '#333')
-		$('.more-button').css('color', '#427fed')
-
-		// set the selected one blue
-		$(this).parent().css('background-color', '#428bca');
-		$(this).parent().css('color', '#FFF')
-		$(this).parent().find('.location').css('color', '#FFF')
-		$(this).parent().find('.date').css('color', '#FFF')
-		$(this).parent().find('.more-button').css('color', '#FFF')
-	}
-
-	shift = function(e) {
-		if($(this).parent().find('#title').is(":visible")) {
-			$(this).parent().find('#title').toggle();
-			$(this).css('left', 'auto');
-			$(this).css('right', '0px');
-			$('#settings-button').hide();
-			$('#sidebar').toggle();
-		} else {
-			$(this).parent().find('#title').toggle();
-			$(this).css('left', '0px');
-			$(this).css('right', 'auto');
-			$('#settings-button').show();
-			$('#sidebar').toggle();
-		}
-	}
-
-	hide_show = function(e) {
-		$('#play-wrapper').toggle();
-	}
-
-	var old_width = $(window).width();
-	resize = function(e) {
-		if(($(window).width() < 800) && (old_width > 800)) {
-			// window moved from big to small
-			$('#sidebar-button').show();
-			$('#settings-button').show();
-			$('#sidebar-button').css('left', '0px');
-			$('#sidebar-button').css('right', 'auto');
-			$('#title').css('display', 'block');
-			$('#sidebar').hide();
-			$('#play-wrapper').hide();
-			old_width = $(window).width();
-		}
-		if(($(window).width() > 800) && (old_width < 800)) {
-			// window moved from small to big
-			$('#sidebar-button').hide();
-			$('#settings-button').hide();
-			$('#title').css('display', 'block');
-			$('#sidebar').show();
-			$('#play-wrapper').show();
-			old_width = $(window).width();
-		}
-	}
-
-	$('.more-button').click(expand);
-	$('#play').click(play_pause);
-	$('.title').click(highlight);
-	$('#sidebar-button').click(shift);
-	$('#settings-button').click(hide_show);
-	$(window).resize(resize);
-*/
 });

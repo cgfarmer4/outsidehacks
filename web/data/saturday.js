6279e45d9aa5d86f724e0680d2cf318915901a7d
Chase Farmer <cfarmer@fanhattan.com>
2013-08-04 10:13:20 -0700

    update saturday

--- web/data/saturday.js
+++ web/data/saturday.js
@@ -4,45 +4,39 @@ var saturday = [
 		"artists": [
 			{
 				"name" : "The Soft White Sixties",
-				"start_time" : "1:00",
-				"end_time": "1:50",
-				"duration": "50",
-				"hottness": "0.442644"				
+				"start_time" : 60, //"1:00",
+				"end_time": 60+50, //"1:50",
+				"hottness": 0.442644				
 			},
 			{
 				"name" : "Gary Clark Jr.",
-				"start_time" : "2:20",
-				"end_time": "3:10",
-				"duration": "50",
-				"hottness": "0.639995"				
+				"start_time" : 60*2+20, //"2:20",
+				"end_time": 60*3+10, //"3:10",
+				"hottness": 0.639995				
 			},
 			{
 				"name" : "Young the Giant",
-				"start_time" : "5:00",
-				"end_time": "6:00",
-				"duration": "60",
-				"hottness": "0.521379"				
+				"start_time" : 60*3+40, //"3:40",
+				"end_time": 60*4+30,//"4:30",
+				"hottness": 0.521379			
 			},
 			{
 				"name" : "Jurassic 5",
-				"start_time" : "5:00",
-				"end_time": "6:00",
-				"duration": "60",
-				"hottness": "0.544037"				
+				"start_time" : 60*5, //"5:00",
+				"end_time": 60*6, //"6:00",
+				"hottness": 0.544037				
 			},
 			{
 				"name" : "Yeah Yeah Yeahs",
-				"start_time" : "6:30",
-				"end_time": "7:40",
-				"duration": "70",
-				"hottness": "0.74454"				
+				"start_time" : 60*6+30, //"6:30",
+				"end_time": 60*7+40, //"7:40",
+				"hottness": 0.74454				
 			},
 			{
 				"name" : "Nine Inch Nails",
-				"start_time" : "8:25",
-				"end_time": "9:55",
-				"duration": "90",
-				"hottness": "0.748916"				
+				"start_time" : 60*8+25, //"8:25",
+				"end_time": 60*9+55, //"9:55",
+				"hottness": 0.748916				
 			}
 		]
 	},
@@ -51,52 +45,45 @@ var saturday = [
 		"artists": [
 			{
 				"name" : "Bhi Bhiman",
-				"start_time" : "12:00",
-				"end_time": "12:40",
-				"duration": "40",
-				"hottness": "0.467051"				
+				"start_time" : 0, //"12:00",
+				"end_time": 40, //"12:40",
+				"hottness": 0.467051
 			},
 			{
 				"name" : "The Lone Bellow",
-				"start_time" : "1:05",
-				"end_time": "1:45",
-				"duration": "40",
-				"hottness": "0.504294"				
+				"start_time" : 60+5, //"1:05",
+				"end_time": 60+45, //"1:45",
+				"hottness": 0.504294				
 			},
 			{
 				"name" : "Milo Greene",
-				"start_time" : "2:10",
-				"end_time": "2:55",
-				"duration": "45",
-				"hottness": "0.464469"				
+				"start_time" : 60*2+10, //"2:10",
+				"end_time": 60*2+55, //"2:55",
+				"hottness": 0.464469				
 			},
 			{
 				"name" : "The Growlers",
-				"start_time" : "3:25",
-				"end_time": "4:10",
-				"duration": "45",
-				"hottness": "0.540958"				
+				"start_time" : 60*3+25, //"3:25",
+				"end_time": 60*4+10, //"4:10",
+				"hottness": 0.540958				
 			},
 			{
 				"name" : "Thao and The Get Down Stay Down",
-				"start_time" : "4:40",
-				"end_time": "5:30",
-				"duration": "40",
-				"hottness": "0.360448"				
+				"start_time" : 60*4+40, //"4:40",
+				"end_time": 60*5+30, //"5:30",
+				"hottness": 0.360448				
 			},
 			{
 				"name" : "The Tallest Man on Earth",
-				"start_time" : "6:00",
-				"end_time": "6:50",
-				"duration": "50",
-				"hottness": "0.557782"				
+				"start_time" : 60*6, //"6:00",
+				"end_time": 60*6+50, //"6:50",
+				"hottness": 0.557782			
 			},
 			{
 				"name" : "The Head and the Heart",
-				"start_time" : "7:20",
-				"end_time": "8:20",
-				"duration": "60",
-				"hottness": "0.558322"
+				"start_time" : 60*7+20, //"7:20",
+				"end_time": 60*8+20, //"8:20",
+				"hottness": 0.558322
 			}
 
 		]
@@ -106,45 +93,39 @@ var saturday = [
 		"artists": [
 			{
 				"name" : "Social Studies",
-				"start_time" : "12:45",
-				"end_time": "1:25",
-				"duration": "40",
-				"hottness": "0.512025"				
+				"start_time" : 45, //"12:45",
+				"end_time": 85, //"1:25",
+				"hottness": 0.512025				
 			},
 			{
 				"name" : "Atlas Genius",
-				"start_time" : "2:10",
-				"end_time": "2:55",
-				"duration": "45",
-				"hottness": "0.664948"				
+				"start_time" : 60*2+10, //"2:10",
+				"end_time": 60*2+55, //"2:55",
+				"hottness": 0.664948				
 			},
 			{
 				"name" : "Youth Lagoon",
-				"start_time" : "3:40",
-				"end_time": "4:30",
-				"duration": "40",
-				"hottness": "0.510066"				
+				"start_time" : 60*3+40, //"3:40",
+				"end_time": 60*4+30, //"4:30",
+				"hottness": 0.510066				
 			},
 			{
 				"name" : "Baauer",
-				"start_time" : "5:15",
-				"end_time": "6:05",
-				"duration": "50",
-				"hottness": "0.665562"				
+				"start_time" : 60*5+15, //"5:15",
+				"end_time": 60*6+5, //"6:05",
+				"hottness": 0.665562
 			},
 			{
 				"name" : "Grizzly Bear",
-				"start_time" : "6:50",
-				"end_time": "7:50",
-				"duration": "60",
-				"hottness": "0.67618"				
+				"start_time" : 60*6+50, //"6:50",
+				"end_time": 60*7+50, //"7:50",
+				"hottness": 0.67618
 			},
 			{
 				"name" : "Phoenix",
-				"start_time" : "8:40",
-				"end_time": "9:55",
-				"duration": "75",
-				"hottness": "0.751129"				
+				"start_time" : 60*8+40, //"8:40",
+				"end_time": 60*9+55, //"9:55",
+				"hottness": 0.751129				
 			}
 		]
 	},
@@ -153,45 +134,39 @@ var saturday = [
 		"artists": [
 			{
 				"name" : "LoCura",
-				"start_time" : "12:00",
-				"end_time": "12:40",
-				"duration": "40",
-				"hottness": "0.332341"				
+				"start_time" : 0, //"12:00",
+				"end_time": 40, //"12:40",
+				"hottness": 0.332341				
 			},
 			{
 				"name" : "Cherub",
-				"start_time" : "1:25",
-				"end_time": "2:05",
-				"duration": "50",
-				"hottness": "0.584937"				
+				"start_time" : 85, //"1:25",
+				"end_time": 60*2+5, //"2:05",
+				"hottness": 0.584937				
 			},
 			{
 				"name" : "James McCartney",
-				"start_time" : "2:55",
-				"end_time": "3:35",
-				"duration": "40",
-				"hottness": "0.470411"				
+				"start_time" : 60*2+55, //"2:55",
+				"end_time": 60*3+35, //"3:35",
+				"hottness": 0.470411				
 			},
 			{
 				"name" : "Bombino",
-				"start_time" : "4:30",
-				"end_time": "5:10",
-				"duration": "40",
-				"hottness": "0.460574"				
+				"start_time" : 60*4+30, //"4:30",
+				"end_time": 60*5+10, //"5:10",
+				"hottness": 0.460574				
 			},
 			{
 				"name" : "The Mother Hips",
-				"start_time" : "6:05",
-				"end_time": "6:50",
-				"duration": "45",
-				"hottness": "0.48259"				
+				"start_time" : 60*6+5, //"6:05",
+				"end_time": 60*6+50, //"6:50",
+				"hottness": 0.48259		
 			},
 			{
 				"name" : "GRiZ",
-				"start_time" : "7:50",
-				"end_time": "8:35",
-				"duration": "45",
-				"hottness": "0.47625"				
+				"start_time" : 60*7+50, //"7:50",
+				"end_time": 60*8+35, //"8:35",
+				"hottness": 0.47625				
 			}
 		]
 	},
@@ -200,38 +175,33 @@ var saturday = [
 		"artists": [
 			{
 				"name" : "Dub Gabriel",
-				"start_time" : "12:00",
-				"end_time": "1:30",
-				"duration": "90",
-				"hottness": "0.446099"				
+				"start_time" : 0, //"12:00",
+				"end_time": 90, //"1:30",
+				"hottness": 0.446099				
 			},
 			{
 				"name" : "DJ Shotnez (Balkan Beat Box)",
-				"start_time" : "1:30",
-				"end_time": "3:00",
-				"duration": "90",
-				"hottness": "0.26289"				
+				"start_time" : 90, //"1:30",
+				"end_time": 60*3, //"3:00",
+				"hottness": 0.26289				
 			},
 			{
 				"name" : "Sam Spiegel (NASA)",
-				"start_time" : "3:00",
-				"end_time": "4:30",
-				"duration": "90",
+				"start_time" : 60*3, //"3:00",
+				"end_time": 60*4+30, //"4:30",
 				"hottness": "0.341333"
 			},
 			{
 				"name" : "Lazy Rich",
-				"start_time" : "4:30",
-				"end_time": "6:00",
-				"duration": "90",
-				"hottness": "0.44079"				
+				"start_time" : 60*4+30, //"4:30",
+				"end_time": 60*6, //"6:00",
+				"hottness": 0.44079				
 			},
 			{
 				"name" : "aKrafty Kuts",
-				"start_time" : "6:00",
-				"end_time": "8:00",
-				"duration": "120",
-				"hottness": "0.52"				
+				"start_time" : 60*6, //"6:00",
+				"end_time": 60*8, //"8:00",
+				"hottness": 0.52				
 			}
 		]
 	},
@@ -240,38 +210,33 @@ var saturday = [
 		"artists": [
 			{
 				"name" : "The Julie Klausner Cabaret Situation with guests Bridget Everett & The Tender Moments and musical director Jon Spurney",
-				"start_time" : "12:00",
-				"end_time": "1:00",
-				"duration": "60",
-				"hottness": ".35"			
+				"start_time" : 0, //"12:00",
+				"end_time": 60, //"1:00",
+				"hottness": .35			
 			},
 			{
 				"name" : "Competitive Erotic Fan Fiction w/Matt Braunger, Kate Berlant, Jesse Elias, Emily Heller,  Dave Hill, Natasha Leggero, Joe Mande",
-				"start_time" : "1:30",
-				"end_time": "2:45",
-				"duration": "75",
-				"hottness": ".35"				
+				"start_time" : 90, //"1:30",
+				"end_time": 60*2+45, //"2:45",
+				"hottness": .35				
 			},
 			{
 				"name" : "Crash Test with Rob Huebel and guests Natasha Leggero,  Bruce McCulloch and Morgan Murphy",
-				"start_time" : "3:15",
-				"end_time": "4:30",
-				"duration": "75",
-				"hottness": ".35"				
+				"start_time" : 60*3+15, //"3:15",
+				"end_time": 60*4+30, //"4:30",
+				"hottness": .35				
 			},
 			{
 				"name" : "Bridget Everett & The Tender Moments hosted by Natasha Leggero",
-				"start_time" : "5:00",
-				"end_time": "6:15",
-				"duration": "75",
-				"hottness": ".35"				
+				"start_time" : 60*5, //"5:00",
+				"end_time": 60*6+15, //"6:15",
+				"hottness": .35				
 			},
 			{
 				"name" : "Night Train with Wyatt Cenac with guests Matt Braunger, Emily Heller, Joe Mande and Glynn Washington",
-				"start_time" : "6:45",
-				"end_time": "8:00",
-				"duration": "75",
-				"hottness": ".35"				
+				"start_time" : 60*6+45, //"6:45",
+				"end_time": 60*8, //"8:00",
+				"hottness": .35				
 			}
 		]
 	}

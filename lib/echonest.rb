5795ba487dcbe21c23d20e6cbc6284da20326627
Chase Farmer <cfarmer@fanhattan.com>
2013-08-03 19:49:59 -0700

    Majority of hotness ratings

--- lib/echonest.rb
+++ lib/echonest.rb
@@ -6,16 +6,33 @@ API_KEY = 'HHGHYPQC0OMN8TCOA'
 # Echonest API 
 # artist = Echonest::Artist.new(API_KEY, 'Weezer')
 
+#Day
+day = 'sunday'
 # Filename
-filename = 'schemas/friday.json'
+filename = 'schemas/' + day + '.json'
 
 # Read in JSON of artists
 
-artists = JSON.parse(IO.read(filename))
+schedule = JSON.parse(IO.read(filename))
 
 # Inject artists back into hash
+schedule[day].each { |x|  
 
-puts artists
+	puts "X: #{x}"
+
+	x["artists"].each{ |y|
+
+		begin
+		  artist = Echonest::Artist.new(API_KEY, y["name"])
+		rescue Echonest::Error => e
+			"Something happened with echonest, keep trucking #{e}"
+		end
+
+		puts "Hotness for #{y["name"]}, #{artist.hotttnesss}"
+	}
+	
+
+}
 
 # Write out hash back to file
 

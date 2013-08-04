76cdb9be6177a9e4af55dbf18fed9198fe9de0d8
Chase Farmer <cfarmer@fanhattan.com>
2013-08-03 19:03:42 -0700

    fix json formats

--- lib/echonest.rb
+++ lib/echonest.rb
@@ -1,2 +1,21 @@
 require 'echonest-ruby-api'
+require 'json'
+
+API_KEY = 'HHGHYPQC0OMN8TCOA'
+
+# Echonest API 
+# artist = Echonest::Artist.new(API_KEY, 'Weezer')
+
+# Filename
+filename = 'schemas/friday.json'
+
+# Read in JSON of artists
+
+artists = JSON.parse(IO.read(filename))
+
+# Inject artists back into hash
+
+puts artists
+
+# Write out hash back to file
 
